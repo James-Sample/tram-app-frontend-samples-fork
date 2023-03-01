83d0c4c0e1b87988f8c3c4c083ffe521f44a9d8f
James Goodman <jamesgoodman206@gmail.com>
2023-03-01 20:49:19 +0000

    added MetroLink map to homepage

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,11 @@ const HomePage = () => {
   return (
     <div>
       <h2 className="home-header">Home Page</h2>
+      <img
+        className="tram-map"
+        src="/images/metrolink-network-map-feb-2023.jpg"
+        alt="Manchester Metrolink Tram Map"
+      />
     </div>
   );
 };
